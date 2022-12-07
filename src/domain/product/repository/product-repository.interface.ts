8a86c4511f62b1b643b665f73395c1550fde46eb
Lucas Rego <lucas.sr.rg@gmail.com>
2022-12-07 09:29:10 -0300

    feature/product repository

---
+++ src/domain/product/repository/product-repository.interface.ts
@@ -0,0 +1,6 @@
+import RepositoryInterface from '../../@shared/repository/repository-interface';
+import Product from '../entity/product';
+
+type ProductRepositoryInterface = RepositoryInterface<Product>;
+
+export default ProductRepositoryInterface;
