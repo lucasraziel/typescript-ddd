eb74820ce0387e9f151f64910588733143bc4e50
Lucas Rego <lucas.sr.rg@gmail.com>
2022-12-07 12:30:24 -0300

    feature/order repository - create only

---
+++ src/domain/checkout/repository/order-repository.interface.ts
@@ -0,0 +1,6 @@
+import RepositoryInterface from '../../@shared/repository/repository-interface';
+import Order from '../entity/order';
+
+type OrderRepositoryInterface = RepositoryInterface<Order>;
+
+export default OrderRepositoryInterface;
