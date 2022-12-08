dd6762fed8a948456a7188429817498dd0181cfb
Lucas Rego <lucas.sr.rg@gmail.com>
2022-12-08 20:01:44 -0300

    fix/names of customer events

--- src/domain/customer/event/handler/send-console-when-customer-is-created2.handler.ts
+++ src/domain/customer/event/handler/send-console-when-customer-is-created2.handler.ts
@@ -1,13 +1,13 @@
 import EventHandlerInterface from '../../../@shared/event/event-handler.interface';
-import ProductCreatedEvent from '../customer-created.event';
+import CustomerCreatedEvent from '../customer-created.event';
 
 import logger from '../../../../infrastructure/@shared/log/logger';
 
 export default class SendConsoleWhenCustomerIsCreated2Handler
-    implements EventHandlerInterface<ProductCreatedEvent>
+    implements EventHandlerInterface<CustomerCreatedEvent>
 {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    handle(event: ProductCreatedEvent): void {
+    handle(event: CustomerCreatedEvent): void {
         logger.info(`Esse é o segundo console.log do evento: CustomerCreated`);
     }
 }
